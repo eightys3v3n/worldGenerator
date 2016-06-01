d70517ac6b0e9ae18548e5343eddbcc8924d5c29
Terrence <eightys3v3n+github@gmail.com>
2016-06-01 13:10:54 -0600

    BACKGROUND_COLOUR to BG_COLOUR
    and removed size and fillolour from init()

--- world.cpp
+++ world.cpp
@@ -6,8 +6,6 @@
 
 void World::init( long long x, long long y )
 {
-  data[x][y].shape.setSize( sf::Vector2f{ CHUNK_SIZE, CHUNK_SIZE } );
-  data[x][y].shape.setFillColor( sf::BACKGROUND_COLOUR );
   data[x][y].type = 0;
 }
 
