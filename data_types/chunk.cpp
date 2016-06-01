e8e3731fa373fef000000a40fb9965969ea3110b
Terrence <eightys3v3n+github@gmail.com>
2016-06-01 13:12:53 -0600

    partially changing to hexagons

--- data_types/chunk.cpp
+++ data_types/chunk.cpp
@@ -1,1 +1,26 @@
+#ifndef CHUNK_TYPE
+#define CHUNK_TYPE
 
+#include <SFML/Graphics.hpp>
+#include "../defaults.hpp"
+
+struct Chunk
+{
+  Chunk()
+  {
+    shape.setFillColor( sf::BG_COLOUR );
+    shape.setSize( sf::Vector2f{ CHUNK_SIZE, CHUNK_SIZE } );
+    shape.setPoint( 0, sf::Vector2f{ -.5 * 1/3 * CHUNK_SIZE , -.5 * CHUNK_SIZE } );
+    shape.setPoint( 0, sf::Vector2f{ +.5 * 1/3 * CHUNK_SIZE , -.5 * CHUNK_SIZE } );
+    shape.setPoint( 0, sf::Vector2f{ +.5 * CHUNK_SIZE , 0 } );
+    shape.setPoint( 0, sf::Vector2f{ +.5 * 1/3 * CHUNK_SIZE , +.5 * CHUNK_SIZE } );
+    shape.setPoint( 0, sf::Vector2f{ -.5 * 1/3 * CHUNK_SIZE , +.5 * CHUNK_SIZE } );
+    shape.setPoint( 0, sf::Vector2f{ -.5 * CHUNK_SIZE , 0 } );
+    shape.setPoint( 0, sf::Vector2f{ -.5 * 1/3 * CHUNK_SIZE , -.5 * CHUNK_SIZE } );
+  }
+
+  int type = 0;
+  sf::ConvexShape shape;
+};
+
+#endif // CHUNK_TYPE
